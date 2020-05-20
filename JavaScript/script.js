e854477538e6eb8deddca22bd7dbc24c9d8d093a
Shajeel-Afzal <shajeel.afzal@gmail.com>
2020-05-20 16:32:12 +0500

    JavaScript Lecture # 1

---
+++ JavaScript/script.js
@@ -0,0 +1,76 @@
+
+
+// alert("Alert from External JS.");
+
+// This is comment!
+
+
+/*
+
+This is multiple lines 
+comment!
+
+*/
+
+// C++ variables
+
+// datatype (int, double, float, etc) variableName;
+
+// JS variables
+var graphicsCard;
+graphicsCard = "\"RTX 2080ti\"";
+document.write(graphicsCard);
+
+var RAM = 32;
+document.write("<br>");
+document.writeln(RAM);
+
+var RAMString = "32";
+
+document.write("<br>");
+document.write("<br>");
+
+var testVariable;
+document.write(testVariable);
+
+document.write("<br>");
+document.write("<br>");
+
+
+// String concatination
+var firstName = "Shajeel";
+var lastName = "Afzal";
+
+document.write("<br>");
+document.write("<br>");
+
+
+document.write(firstName + " " + lastName);
+
+document.write("<br>");
+document.write("<br>");
+
+var ram1 = 16;
+var ram2 = 16;
+
+document.write(ram1 + ram2);
+
+
+
+
+// JS is a case sensitive language.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
