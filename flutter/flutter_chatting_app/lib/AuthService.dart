eb0c30027aa7aea98a18e52071dd21c78c84b2b3
Shajeel Afzal <shajeel.afzal@gmail.com>
2020-07-13 10:41:32 +0500

    added code to signin with email

--- flutter/flutter_chatting_app/lib/AuthService.dart
+++ flutter/flutter_chatting_app/lib/AuthService.dart
@@ -1,6 +1,30 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
+  Future<void> loginUser(String email, String password) async {
+    AuthResult authResult = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+
+    print(authResult);
+
+    // await FirebaseAuth.instance.verifyPhoneNumber(
+    //   phoneNumber: "+923003419643",
+    //   timeout: Duration(seconds: 10),
+    //   verificationCompleted: (phoneAuthCredential) {
+    //     print("verificationCompleted Called!");
+    //   },
+    //   verificationFailed: (error) {
+    //     print("verification failed!");
+    //   },
+    //   codeSent: (verificationId, [forceResendingToken]) {
+    //     print("codeSent");
+    //   },
+    //   codeAutoRetrievalTimeout: (verificationId) {
+    //     print("codeAutoRetrievalTimeout called!");
+    //   },
+    // );
+  }
+
   Future<bool> isUserLogin() async {
     FirebaseUser firebaseUser = await FirebaseAuth.instance.currentUser();
 
--- flutter/flutter_chatting_app/lib/LoginScreen.dart
+++ flutter/flutter_chatting_app/lib/LoginScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chatting_app/AuthService.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
